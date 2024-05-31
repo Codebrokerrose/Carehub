f2f27938719a6423aafa45f0a8336b80300794cf
fatemaker254 <dassoham254@gmail.com>
2024-05-31 21:47:09 +0530

    Plant Info and ContactUs updated

--- contactus/Contact.php
+++ contactus/Contact.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,10 +10,13 @@
     <link rel="stylesheet" href="Contact.css">
     <link rel="stylesheet" href="myProjects/webProject/icofont/css/icofont.min.css">
     <script src="https://kit.fontawesome.com/f75e97ac39.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
 <body>
-    
+
     <section id="header">
         <!-- navigation -->
         <?php include 'C:\xampp\htdocs\carehub\home\nav.php'; ?>
@@ -47,10 +51,13 @@
         </div>
         <div class="map">
             <!--adding the google map-->
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.0081103378798!2d88.28075391429411!3d22.541368939798513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a02790ae49907e5%3A0xa0fa815e35df395e!2sMallick%20Para%20Basti%2C%20Garden%20Reach%2C%20Kolkata%2C%20West%20Bengal%20700024!5e0!3m2!1sen!2sin!4v1659711929694!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.0081103378798!2d88.28075391429411!3d22.541368939798513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a02790ae49907e5%3A0xa0fa815e35df395e!2sMallick%20Para%20Basti%2C%20Garden%20Reach%2C%20Kolkata%2C%20West%20Bengal%20700024!5e0!3m2!1sen!2sin!4v1659711929694!5m2!1sen!2sin"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
-    <section id="form-details">
+    <section id="form-details" style=" margin:0px ">
         <form action="">
             <span>LEAVE A MESSAGE</span>
             <h2>We would love to hear from you</h2>
@@ -64,17 +71,20 @@
             <div class="circular-image">
                 <img src="/carehub/usericon.png" alt="">
                 <p><span>Soham Das</span>Ceo <br>Phone: +91 81000XXXXX
-                <br>E-mail:contact@abcd.com</p>
+                    <br>E-mail:contact@abcd.com
+                </p>
             </div>
             <div>
                 <img src="/carehub/usericon.png" alt="">
                 <p><span>Raunak Kanji</span>Ceo <br>Phone: +91 81000XXXXX
-                <br>E-mail:contact@abcd.com</p>
+                    <br>E-mail:contact@abcd.com
+                </p>
             </div>
             <div>
                 <img src="/carehub/usericon.png" alt="">
                 <p><span>Abhishek Balmiki</span>Ceo <br>Phone: +91 81000XXXXX
-                <br>E-mail:contact@abcd.com</p>
+                    <br>E-mail:contact@abcd.com
+                </p>
             </div>
         </div>
     </section>
@@ -92,4 +102,5 @@
     </section>
     <?php include 'C:\xampp\htdocs\carehub\home\footer.php'; ?>
 </body>
+
 </html>
